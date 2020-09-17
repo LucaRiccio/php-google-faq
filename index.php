f8a9a406866b9b4db3d8df8a9a44f0cc86853720
LucaRiccio <lucariccio888@gmail.com>
2020-09-17 16:38:24 +0200

    Bozza HTML

---
+++ index.php
@@ -0,0 +1,31 @@
+<!-- Riscrivere questa pagina del sito google https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte. Gestire il “Database”
+e la visualizzazione di queste domande e risposte con PHP. -->
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>php-google-faq</title>
+  </head>
+  <body>
+    <header>
+
+      <div class="header-top">
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="Logo Google">
+        <h2>Privacy e termini</h2>
+      </div>
+
+      <div class="header-bottom">
+        <p>Introduzione</p>
+        <p>Norme</p>
+        <p>Termini di servizio</p>
+        <p>Tecnologie</p>
+        <p>Domande frequenti</p>
+      </div>
+      <hr>
+    </header>
+
+  </body>
+</html>
